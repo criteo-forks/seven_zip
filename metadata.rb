f91f6841ebdc65349a93fa1b9e7772d54deab1ab
Robert Veznaver <r.veznaver@criteo.com>
2016-02-15 13:45:10 +0100

    Create fork version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'sneal@sneal.net'
 license          'Apache 2.0'
 description      'Installs/Configures the 7-zip file archiver'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.4'
+version          '1.0.42'
 supports         'windows'
 
 depends          'windows', '>= 1.2.2'
